2e090f03eb0a8d4592d65d8884c4e765dd720eae
Quang Huy <tqhuy9701@gmail.com>
2023-05-30 17:23:00 +0700

    Update

--- Assets/Scripts/Bridge/Step/Step.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Step : MonoBehaviour
-{
-    public int stepteam = -1;
-    public int currentindex;
-    private List<int> charmet = new List<int>();
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# FinalBridgeRace_HuyTQ
- 
